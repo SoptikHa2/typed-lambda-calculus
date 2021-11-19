a7fa09c774fc7c2767461a0bb55e722dfb8309dd
Petr Šťastný <stastpe8@fit.cvut.cz>
2021-11-19 12:33:49 +0100

    Fixed evaluation for nested lambda applications

--- Evaluation.hs
+++ Evaluation.hs
@@ -11,7 +11,8 @@ normalize (Variable v) rr = case lookup v rr of
 normalize (Application (LambdaAbstraction param t expr) apl) rr = let rr' = (param, apl) : rr in
     normalize expr rr'
 normalize (Application (AnnotatedExpression _ e) apl) rr = normalize (Application (normalize e rr) apl) rr
-normalize (Application _ _) _ = error "Application requires lambda expression as it's first parameter"
+normalize (Application expr apl) rr = case normalize expr rr of
+  LambdaAbstraction param t expr' -> normalize (Application (LambdaAbstraction param t expr') apl) rr
+  expr' -> error $ "Left hand of applicatio doesn't contain lambda abstraction even after normalization. Received " ++ show expr'
 normalize (LambdaAbstraction param t expr) rr = LambdaAbstraction param t $ normalize expr rr
 normalize (AnnotatedExpression _ e) rr = normalize e rr
-normalize exp _ = error $ "Application didn't recognize expression " ++ show exp ++ ". Try desugaring first."
