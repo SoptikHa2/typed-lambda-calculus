ee09a40dc2844e095f42e7fb1efc6aba98b3856c
Petr Šťastný <stastpe8@fit.cvut.cz>
2021-11-19 11:56:19 +0100

    Added evaluation support for annotated expressions

--- Evaluation.hs
+++ Evaluation.hs
@@ -16,6 +16,7 @@ normalize (Variable v) rr = case _replacementRule v rr of
     Nothing -> Variable v
 normalize (Application (LambdaAbstraction param t expr) apl) rr = let rr' = (param, apl) : rr in
     normalize expr rr'
+normalize (Application (AnnotatedExpression _ e) apl) rr = normalize (Application (normalize e rr) apl) rr
 normalize (Application _ _) _ = error "Application requires lambda expression as it's first parameter"
 normalize (LambdaAbstraction param t expr) rr = LambdaAbstraction param t $ normalize expr rr
 normalize (AnnotatedExpression _ e) rr = normalize e rr
