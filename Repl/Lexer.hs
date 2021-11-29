c0e12b4e34b24ff4893e319c0d423a8b874f2ff1
Petr Šťastný <stastpe8@fit.cvut.cz>
2021-11-29 13:35:04 +0100

    Finixhed lexer

--- Repl/Lexer.hs
+++ Repl/Lexer.hs
@@ -2,23 +2,57 @@ module Repl.Lexer where
 
 import Text.ParserCombinators.ReadP
 import Repl.Tokens( Token(..) )
-import Core.Expression ( Expression(..) )
+import Data.Char (isLetter)
+import GHC.Unicode (isLower)
+import Control.Applicative
 
-expression :: ReadP Expression
-expression = error "unimplemented"
+isLambda :: Char -> Bool
+isLambda char = char `elem` "/\\λ"
 
-lambdaAbstraction :: ReadP Expression
-lambdaAbstraction = error "unimplemented"
+isIdentifier :: Char -> Bool
+-- todo: there has to be a better way
+isIdentifier char = isLetter char && isLower char && not (isLambda char)
 
-application :: ReadP Expression
-application = do
-    abstraction <- lambdaAbstraction
-    value <- expression
-    return $ Application abstraction value
+identifier :: ReadP Token
+identifier = do
+    letter <- count 1 (satisfy (\char -> isLetter char && not (isLambda char)))
+    return $ Identifier letter
 
+lambda :: ReadP Token
+lambda = do
+    satisfy isLambda
+    return Lambda
 
-variable :: ReadP Expression
-variable = do
-    skipSpaces 
-    return $ Variable $ munch1 isLetter
+dot :: ReadP Token
+dot = do
+    char '.'
+    return Dot
 
+leftParenthesis :: ReadP Token
+leftParenthesis = do
+    char '('
+    return LeftParenthesis 
+
+rightParenthesis :: ReadP Token
+rightParenthesis = do
+    char ')'
+    return RightParenthesis 
+
+colon :: ReadP Token
+colon = do
+    char ':'
+    return Colon
+
+arrow :: ReadP Token
+arrow = do
+    string "->"
+    return Arrow
+
+tau :: ReadP Token
+tau = do
+    char 't' <|> char 'T' <|> char 'τ'
+    return Tau
+
+lexer :: ReadP [Token]
+lexer =
+    sepBy (choice [identifier, lambda, dot, leftParenthesis, rightParenthesis, colon, arrow, tau]) skipSpaces
